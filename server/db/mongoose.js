4e3976e436ed6a8be195e006d65350cab2387881
Michael Yiu <onew1ng3d@gmail.com>
2018-10-19 11:09:54 -0400

    test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
