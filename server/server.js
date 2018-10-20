7414d05bc0787d9d015dfd38e28736cf3acbb5f7
Michael Yiu <onew1ng3d@gmail.com>
2018-10-20 00:50:35 -0400

    setting up remove route

--- server/server.js
+++ server/server.js
@@ -33,6 +33,21 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    //get the id
+
+    //validate the id -> not valid? return 404
+
+    //remove todo by id
+        //success
+            //if no doc send 404
+            //if doc send doc with 200
+        //error
+            //400 with empty body
+})
+
+
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
