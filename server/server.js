e02bca33e0caec324671a3a88801780a4caba922
Michael Yiu <onew1ng3d@gmail.com>
2018-10-21 12:49:51 -0400

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+require('./config/config')
+
+
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,7 +14,7 @@ let { User } = require('./models/user');
 
 
 let app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(bodyParser.json());
 
